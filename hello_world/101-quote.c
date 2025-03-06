809714883d6816874af6ff5898c85c3cf7a1a209
donnasaud <donnaalmadani@gmail.com>
2025-03-06 19:26:46 +0000

    task 8 second try

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,19 +1,16 @@
+#include <stdio.h>
 #include <unistd.h>
 
 /**
- * main - Entry point of the program
+ **main - Entry point
  *
- * Description: Prints "and that piece of art is useful"
- *              - Dora Korpar, 2015-10-19 to standard error.
- *              Does not use printf or puts.
- * Return: Always returns 1 (error)
- */
+ **description: 'to display a string using the write method'
+ *
+ **Return: Always 0 (Success)
+ **/
+
 int main(void)
 {
-	char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	write(2, message, 59);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-
