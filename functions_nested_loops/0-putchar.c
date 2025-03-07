d56c45845c6485a8a0b8a15d918c91da4ee424e3
donnasaud <donnaalmadani@gmail.com>
2025-03-07 19:53:41 +0000

    0. _putchar

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "_putchar" followed by a new line
+ * using only the _putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
+
