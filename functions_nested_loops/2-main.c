33e363a1d2ed2a7e97e278b15e16b3ed7a4ef3e4
donnasaud <donnaalmadani@gmail.com>
2025-03-07 21:04:17 +0000

    task 2

---
+++ functions_nested_loops/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main - Entry point to test print_alphabet_x10 function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    print_alphabet_x10(); /* Call function */
+    return (0);
+}
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
-void print_alphabet(void); /* Function prototype */
+int _putchar(char c);  /* Ensure this prototype exists */
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
 
