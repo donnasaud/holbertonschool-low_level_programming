f88673549435a416e496c1f2a4efaa047c856a86
donnasaud <donnaalmadani@gmail.com>
2025-03-08 17:11:53 +0000

    second try task 7 C

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,14 +8,18 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit;
+	int last_digit;
 
-    /* Get the absolute value of the last digit */
-    last_digit = n % 10;
-    if (last_digit < 0)
-        last_digit *= -1;
+	/* Extract the last digit */
+	last_digit = n % 10;
 
-    _putchar(last_digit + '0'); /* Convert to char and print */
-    return (last_digit);
+	/* Convert negative last digit to positive */
+	if (last_digit < 0)
+		last_digit *= -1;
+
+	/* Print the last digit */
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
 
