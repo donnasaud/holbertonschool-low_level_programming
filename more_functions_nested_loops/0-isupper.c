9e2c0080c95eb668560a7950afa3991871eaad6b
donnasaud <donnaalmadani@gmail.com>
2025-03-13 20:28:01 +0000

    retry task 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -8,8 +8,13 @@
  */
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z') /* Checks if c is between A and Z */
-        return (1);
-    return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 
