da0360f3adadc29a9b0156186f717256ba16dbe3
donnasaud <donnaalmadani@gmail.com>
2025-03-14 22:22:44 +0000

    task 12 in C functions 2nd try

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,29 +1,29 @@
 #include "main.h"
 
 /**
- * print_number - Prints an integer using _putchar.
- * @n: The integer to print.
+ * print_number - Prints an integer using _putchar
+ * @n: The integer to print
  */
 void print_number(int n)
 {
-    unsigned int num;
+	unsigned int num;
 
-    /* Handle negative numbers */
-    if (n < 0)
-    {
-        _putchar('-');
-        num = -n;
-    }
-    else
-    {
-        num = n;
-    }
+	/* Handle negative numbers */
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
 
-    /* Recursively print digits */
-    if (num / 10)
-        print_number(num / 10);
+	/* Recursively print digits */
+	if (num / 10)
+		print_number(num / 10);
 
-    /* Print the last digit */
-    _putchar((num % 10) + '0');
+	/* Print the last digit */
+	_putchar((num % 10) + '0');
 }
 
