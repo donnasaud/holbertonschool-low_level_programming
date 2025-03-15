666b875d35f2ae9b7fcf431d7e565b2d34033c55
donnasaud <donnaalmadani@gmail.com>
2025-03-15 06:30:51 +0000

    task 5 4th try

--- more_malloc_free/101-mul.c
+++ more_malloc_free/101-mul.c
@@ -9,17 +9,17 @@
  */
 int is_number(char *s)
 {
-    int i = 0;
+	int i = 0;
 
-    if (!s || s[0] == '\0')
-        return (0);
-    while (s[i])
-    {
-        if (s[i] < '0' || s[i] > '9')
-            return (0);
-        i++;
-    }
-    return (1);
+	if (!s || s[0] == '\0')
+		return (0);
+	while (s[i])
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 /**
@@ -30,60 +30,62 @@ int is_number(char *s)
  */
 char *multiply(char *num1, char *num2)
 {
-    int len1 = 0, len2 = 0, i, j, carry, n1, n2, sum;
-    int *result;
-    char *final_result;
+	int len1 = 0, len2 = 0, i, j, carry, n1, n2, sum;
+	int *result;
+	char *final_result;
 
-    while (num1[len1])
-        len1++;
-    while (num2[len2])
-        len2++;
+	while (num1[len1])
+		len1++;
+	while (num2[len2])
+		len2++;
 
-    if ((num1[0] == '0' && num1[1] == '\0') || (num2[0] == '0' && num2[1] == '\0'))
-    {
-        final_result = malloc(2);
-        if (!final_result)
-            return (NULL);
-        final_result[0] = '0';
-        final_result[1] = '\0';
-        return (final_result);
-    }
+	/* Handle multiplication by zero */
+	if ((num1[0] == '0' && num1[1] == '\0') ||
+	    (num2[0] == '0' && num2[1] == '\0'))
+	{
+		final_result = malloc(2);
+		if (!final_result)
+			return (NULL);
+		final_result[0] = '0';
+		final_result[1] = '\0';
+		return (final_result);
+	}
 
-    result = calloc(len1 + len2, sizeof(int));
-    if (!result)
-        return (NULL);
+	result = calloc(len1 + len2, sizeof(int));
+	if (!result)
+		return (NULL);
 
-    for (i = len1 - 1; i >= 0; i--)
-    {
-        n1 = num1[i] - '0';
-        carry = 0;
-        for (j = len2 - 1; j >= 0; j--)
-        {
-            n2 = num2[j] - '0';
-            sum = n1 * n2 + result[i + j + 1] + carry;
-            carry = sum / 10;
-            result[i + j + 1] = sum % 10;
-        }
-        result[i + j + 1] += carry;
-    }
+	for (i = len1 - 1; i >= 0; i--)
+	{
+		n1 = num1[i] - '0';
+		carry = 0;
+		for (j = len2 - 1; j >= 0; j--)
+		{
+			n2 = num2[j] - '0';
+			sum = n1 * n2 + result[i + j + 1] + carry;
+			carry = sum / 10;
+			result[i + j + 1] = sum % 10;
+		}
+		result[i + j + 1] += carry;
+	}
 
-    final_result = malloc(len1 + len2 + 1);
-    if (!final_result)
-    {
-        free(result);
-        return (NULL);
-    }
+	final_result = malloc(len1 + len2 + 1);
+	if (!final_result)
+	{
+		free(result);
+		return (NULL);
+	}
 
-    j = 0;
-    for (i = 0; i < len1 + len2; i++)
-    {
-        if (!(j == 0 && result[i] == 0))
-            final_result[j++] = result[i] + '0';
-    }
-    final_result[j] = '\0';
+	j = 0;
+	for (i = 0; i < len1 + len2; i++)
+	{
+		if (!(j == 0 && result[i] == 0))
+			final_result[j++] = result[i] + '0';
+	}
+	final_result[j] = '\0';
 
-    free(result);
-    return (j == 0) ? "0" : final_result;
+	free(result);
+	return (j == 0) ? "0" : final_result;
 }
 
 /**
@@ -94,27 +96,28 @@ char *multiply(char *num1, char *num2)
  */
 int main(int argc, char *argv[])
 {
-    char *result;
+	char *result;
 
-    if (argc != 3 || !is_number(argv[1]) || !is_number(argv[2]))
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	if (argc != 3 || !is_number(argv[1]) || !is_number(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    result = multiply(argv[1], argv[2]);
-    if (!result)
-    {
-        printf("Error\n");
-        exit(98);
-    }
+	result = multiply(argv[1], argv[2]);
+	if (!result)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-    printf("%s\n", result);
+	printf("%s\n", result);
 
-    if (result[0] != '0' || result[1] != '\0')
-        free(result);
+	/* Free allocated memory */
+	if (result[0] != '0' || result[1] != '\0')
+		free(result);
 
-    return (0);
+	return (0);
 }
 
 
