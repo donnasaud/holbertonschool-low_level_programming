3927bcd49409c65d195323dc545d98fe4ab578be
donnasaud <donnaalmadani@gmail.com>
2025-03-15 20:17:21 +0000

    task 0 in C typedef

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure for a dog
+ * @name: Pointer to the dog's name
+ * @age: Dog's age (float)
+ * @owner: Pointer to the owner's name
+ *
+ * Description: This structure stores information about a dog.
+ */
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif /* DOG_H */
+
