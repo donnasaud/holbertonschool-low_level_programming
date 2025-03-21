fe1ad261bddc537693f4c39f639c46f7eeca59a5
donnasaud <donnaalmadani@gmail.com>
2025-03-21 22:21:27 +0000

    task 100 final try

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,20 +1,18 @@
 section .data
-    hello_msg db "Hello, World", 10  ; 10 is newline '\n'
+    hello_msg db "Hello, World", 10
     msg_len equ $ - hello_msg
 
 section .text
     global _start
 
 _start:
-    ; write syscall: sys_write(fd=1, buf=hello_msg, count=msg_len)
-    mov rax, 1          ; syscall number for write
-    mov rdi, 1          ; file descriptor (stdout)
-    mov rsi, hello_msg  ; pointer to message
+    mov rax, 1          ; syscall: write
+    mov rdi, 1          ; stdout
+    mov rsi, hello_msg  ; message address
     mov rdx, msg_len    ; message length
     syscall
 
-    ; exit syscall: sys_exit(status=0)
-    mov rax, 60         ; syscall number for exit
-    xor rdi, rdi        ; exit code 0
+    mov rax, 60         ; syscall: exit
+    xor rdi, rdi        ; status: 0
     syscall
 
