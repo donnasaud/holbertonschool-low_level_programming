676d8b80f5dd0f7fe72425cc598e69b4cb6a78c4
donnasaud <donnaalmadani@gmail.com>
2025-04-04 18:15:35 +0000

    task 0 - single linked lists in C

---
+++ singly_linked_lists/lists.h
@@ -0,0 +1,16 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>  /* for size_t */
+
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
+size_t print_list(const list_t *h);  /* Function prototype */
+
+#endif
+
