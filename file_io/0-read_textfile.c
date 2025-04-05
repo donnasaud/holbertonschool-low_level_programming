282255aaf14203344b5853b0db719d56b1748234
donnasaud <donnaalmadani@gmail.com>
2025-04-05 20:39:35 +0000

    task 0 - fix better code 2nd try

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,13 +4,15 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints it to the POSIX standard
+ * output.
  * @filename: Name of the file to read.
  * @letters: Number of letters it should read and print.
  *
  * Return: Actual number of letters it could read and print.
  *         0 if the file can't be opened or read, or if write fails.
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
