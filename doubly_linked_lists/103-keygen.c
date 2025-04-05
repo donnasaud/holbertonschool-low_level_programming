b8e84c082ca9e00296d8247ea9df0ca072d8b3aa
donnasaud <donnaalmadani@gmail.com>
2025-04-05 15:57:52 +0000

    task 11 - fixing error

--- doubly_linked_lists/103-keygen.c
+++ doubly_linked_lists/103-keygen.c
@@ -1,21 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
-/**
- * main - Dummy keygen that prints a test key
- * @argc: number of arguments
- * @argv: array of arguments
- *
- * Return: 0 on success
- */
 int main(int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		printf("Usage: %s username\n", argv[0]);
-		return (1);
-	}
+    char *username;
+    int len, total, i;
+    char key[7];
+    char *charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-	printf("123456\n"); /* Replace this with real keygen logic later */
-	return (0);
+    if (argc != 2)
+    {
+        printf("Usage: %s username\n", argv[0]);
+        return (1);
+    }
+
+    username = argv[1];
+    len = strlen(username);
+
+    key[0] = charset[(len ^ 59) & 63];
+
+    total = 0;
+    for (i = 0; i < len; i++)
+        total += username[i];
+    key[1] = charset[(total ^ 79) & 63];
+
+    total = 1;
+    for (i = 0; i < len; i++)
+        total *= username[i];
+    key[2] = charset[(total ^ 85) & 63];
+
+    srand(username[0] ^ 14);
+    key[3] = charset[rand() & 63];
+
+    total = 0;
+    for (i = 0; i < len; i++)
+    {
+        if (username[i] > total)
+            total = username[i];
+    }
+    srand(total ^ 229);
+    key[4] = charset[rand() & 63];
+
+    total = 0;
+    for (i = 0; i < len; i++)
+        total += username[i] * username[i];
+    key[5] = charset[(total ^ 239) & 63];
+
+    key[6] = '\0';
+    printf("%s\n", key);
+    return (0);
 }
+
 
