f29ef3cd931a40afdccf15c794aa9b8ef4a6c906
donnasaud <donnaalmadani@gmail.com>
2025-04-22 21:16:06 +0000

    task 3 cat

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcmp - Compares two strings
+ * @s1: First string
+ * @s2: Second string
+ *
+ * Return: 0 if equal, negative if s1 < s2, positive if s1 > s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && *s2 != '\0')
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
+
