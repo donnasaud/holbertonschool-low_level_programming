477f843aa7e032d860ee4fe20cca3a9565be29e0
donnasaud <donnaalmadani@gmail.com>
2025-04-22 21:18:38 +0000

    task 4 in cat

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - Reverses the content of an array of integers
+ * @a: Array of integers
+ * @n: Number of elements in the array
+ *
+ * Return: Nothing
+ */
+void reverse_array(int *a, int n)
+{
+	int i, temp;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = temp;
+	}
+}
+
