a2103c8080481da5c4c83de814244898b6ee8a77
donnasaud <donnaalmadani@gmail.com>
2025-11-14 21:36:24 +0000

    fixes bitty

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of arguments passed to the program
- * @argc: number of arguments
- * @argv: array of arguments (not used)
- *
- * Return: 0
- */
-int main(int argc, char __attribute__((unused)) **argv)
+ * main - Prints the number of arguments passed to it
+ * @argc: The number of arguments supplied to the program
+ * @argv: An array of pointers to the arguments
+ * Return: Always 0
+*/
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-    printf("%d\n", argc - 1);
-    return (0);
-}
+printf("%d\n", argc - 1);
 
+return (0);
+}
